7ef65a0fe44be768ea4ee2b9a1945fd84c351d9a
JanAnkerKristensen <jaakr@tdcnet.dk>
2025-03-25 11:46:55 +0000

    trip constant

--- dagster_university/dagster_essentials/dagster_essentials/assets/trips.py
+++ dagster_university/dagster_essentials/dagster_essentials/assets/trips.py
@@ -1,7 +1,22 @@
+TAXI_ZONES_FILE_PATH = "data/raw/taxi_zones.csv"
+TAXI_TRIPS_TEMPLATE_FILE_PATH = "data/raw/taxi_trips_{}.parquet"
+
+TRIPS_BY_AIRPORT_FILE_PATH = "data/outputs/trips_by_airport.csv"
+TRIPS_BY_WEEK_FILE_PATH = "data/outputs/trips_by_week.csv"
+MANHATTAN_STATS_FILE_PATH = "data/staging/manhattan_stats.geojson"
+MANHATTAN_MAP_FILE_PATH = "data/outputs/manhattan_map.png"
+
+REQUEST_DESTINATION_TEMPLATE_FILE_PATH = "data/outputs/{}.png"
+
+DATE_FORMAT = "%Y-%m-%d"
+
+START_DATE = "2023-01-01"
+END_DATE = "2023-04-01"
+
 import requests
-from . import constants
 import dagster as dg
 
+@dg.asset
 def taxi_trips_file() -> None:
     """
       The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
